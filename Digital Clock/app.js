319657ef77da283a363c0ff6fe778d3b6db632d5
mohammad taha abdollah <75626161+tahaabdollah2002@users.noreply.github.com>
2022-07-14 01:34:17 +0430

    Add full source code.

---
+++ Digital Clock/app.js
@@ -0,0 +1,29 @@
+setInterval(function()  {
+    const clock = document.querySelector(".display");
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hr = time.getHours();
+    var day = "AM";
+    if(hr > 0)
+    {
+        day = "PM";
+    }
+    if(hr==0){
+        hr=12;
+    }
+    if(sec < 10)
+    {
+        sec = '0'+sec;
+    }
+    if(min < 10)
+    {
+        min = '0'+min;
+    }
+    if(hr < 10)
+    {
+        hr = '0'+hr;
+    }
+    
+    clock.textContent = hr+':'+min+":"+sec+" - "+day
+}, 1000);
